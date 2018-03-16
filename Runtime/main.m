89d41aa89ee8ab0c27fe933ea1e3f0e4acdddae7
qiqncaox <2431301938@qq.com>
2018-03-16 09:37:13 +0800

    Initial Commit

---
+++ Runtime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Runtime
+//
+//  Created by yxiang on 2018/3/16.
+//  Copyright © 2018年 qiancaox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
